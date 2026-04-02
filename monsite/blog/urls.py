3888a17f2759b1326b7be32e85a6d43736bee147
enric tavares <enrictavares01@gmail.com>
2026-04-02 15:55:48 +0200

    Hello

--- monsite/blog/urls.py
+++ monsite/blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('submit_payroll/', views.submit_payroll, name='submit_payroll'),
+    path('confirmation/', views.confirmation, name='confirmation'),
 ]
--- monsite/blog/views.py
+++ monsite/blog/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'blog/home.html')
+
+def submit_payroll(request):
+    if request.method == 'POST':
+        # Traiter les données du formulaire
+        return redirect('confirmation')
+    return redirect('home')
+
+def confirmation(request):
+    return render(request, 'blog/confirmation.html')
